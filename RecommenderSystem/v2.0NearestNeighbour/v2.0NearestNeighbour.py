e102f2ab2729c814fa62bd98f18e798e98982846
mortenm12 <mortenm12@hotmail.com>
2016-11-02 14:17:17 +0100

    Adding results for k = 80

--- RecommenderSystem/v2.0NearestNeighbour/v2.0NearestNeighbour.py
+++ RecommenderSystem/v2.0NearestNeighbour/v2.0NearestNeighbour.py
@@ -51,7 +51,7 @@ def k_nearest_neighbour(movie, user1, k, ratings, users, weight_matrix):
 
 
 def rate(movie, user, users, ratings, weight_matrix):
-    weight_rating_tuples = k_nearest_neighbour(movie, user, 40, ratings, users, weight_matrix)
+    weight_rating_tuples = k_nearest_neighbour(movie, user, 80, ratings, users, weight_matrix)
     sum1 = 0
     sum2 = 0
     for x in weight_rating_tuples:
@@ -75,7 +75,7 @@ def format_time(t):
         return "{:02d}".format(int(h)) + ":" + "{:02d}".format(int(m)) + ":" + "{:02d}".format(int(s))
 
 
-def calculate_weight_matrix(movies, ratings, users):
+def calculate_weight_matrix(movies, ratings, users, x):
     t_start = time.time()
     weight_matrix = []
     for i in range(len(movies)):
@@ -87,7 +87,7 @@ def calculate_weight_matrix(movies, ratings, users):
         t_current = time.time()
         t_elapsed = t_current - t_start
         t_remaining = (t_elapsed * len(movies)) / user1 - t_elapsed
-        print(round((user1 / len(users)) * 100, 1), "% tid brugt: ", format_time(t_elapsed), " tid tilbage: ",
+        print(x, " Calculating Weight", round((user1 / len(users)) * 100, 1), "% tid brugt: ", format_time(t_elapsed), " tid tilbage: ",
               format_time(t_remaining))
         for user2 in users:
             if user1 != user2:
@@ -104,7 +104,7 @@ for x in range(1, 6):
 
     i = 0
     rated = ratings
-    weight_matrix = calculate_weight_matrix(movies, ratings, users)
+    weight_matrix = calculate_weight_matrix(movies, ratings, users, x)
     starting_time = time.time()
     for user in users:
         i += 1
@@ -113,7 +113,7 @@ for x in range(1, 6):
         elapsed_time = current_time - starting_time
         remaining_time = ((elapsed_time * len(users)) / i) - elapsed_time
 
-        print(round((i / len(users)) * 100, 1), "% tid brugt: ", format_time(elapsed_time), " tid tilbage: ", format_time(remaining_time))
+        print(x, " Rater", round((i / len(users)) * 100, 1), "% tid brugt: ", format_time(elapsed_time), " tid tilbage: ", format_time(remaining_time))
 
         for movie in movies:
             if ratings[user - 1][movie - 1] == 0.0:
@@ -136,7 +136,7 @@ for x in range(1, 6):
 
     for user in users:
         i += 1
-        print(round((i / len(users)) * 100, 1), "%")
+        print("Writing", round((i / len(users)) * 100, 1), "%")
         output.write("{:>5}".format(user) + ", ")
         for movie in movies:
             output.write("{: .2f}".format(rated[user - 1][movie - 1]) + (", " if movie < len(movies) else ""))
