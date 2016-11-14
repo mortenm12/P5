7e3a8e252193654cc59f3afcf05c138b40280225
moller93 <jensen.rasmusmoller@gmail.com>
2016-11-14 13:18:17 +0100

    Refactored Matrix Factorization V.2

--- RecommenderSystem/Matrix Factorization V.2/MatrixFactorization.py
+++ RecommenderSystem/Matrix Factorization V.2/MatrixFactorization.py
@@ -9,49 +9,6 @@ import Evaluation
 import copy
 
 
-# Algorithm to write a matrix to a file
-def write_numpy_matrix(m, file, test_set):
-    result = open("Output/" + test_set + "/" + file, "w")
-    result.write(" ID , " + ", ".join(str(x) for x in range(1, len(m[0]) + 1)) + "\n")
-    for i in range(0, len(m)):
-        result.write("{:>4d}, ".format(i + 1))
-        result.write(", ".join(["{: .2f}".format(x) for x in m[i]]))
-        result.write("\n")
-
-    if not result.closed:
-        result.close()
-
-
-def write_factor_matrix(m, file, test_set):
-    result = open("Output/" + test_set + "/" + file, "w")
-    for i in range(0, len(m)):
-        result.write("{:>4d}, ".format(i + 1))
-        result.write(", ".join(["{: .2f}".format(x) for x in m[i]]))
-        result.write("\n")
-
-    if not result.closed:
-        result.close()
-
-
-# Method to calculate the recommendations from the P and Q matrices.
-def calculate_recommendations(P, Q, R, test_set):
-    recommendation_file = open("Output/" + test_set + "/UserRecommendations", "w")
-
-    # For each user vector find the most similar item vector by calculating the dot products.
-    a = 0
-    most_accurate_product = 0
-    for i in range(0, len(P)):
-        for j in range(0, len(Q)):
-            result = numpy.dot(P[i, :], Q[j, :])
-            if result > most_accurate_product and R[i][j] == 0.0:
-                most_accurate_product = result
-                a = j
-        recommendation_file.write("User: " + str(i) + ", Recommended movie: " + str(a) + "Rating: " + str(most_accurate_product) + "\n")
-
-    if not recommendation_file.closed:
-        recommendation_file.close()
-
-
 # The factorization algorithm.
 # The point of this algorithm is to stepwise approximate P and Q so that PxQ approximates R.
 # Takes 4 parameters:
@@ -68,7 +25,7 @@ def matrix_factorization(P, Q, R, K, steps=5000, alpha=0.07, beta=0.02):
     # Transposes the Q matrix, so the dot product of P and Q can be taken.
     Q = Q.T
 
-    prev_e = 0
+    prev_error = 0
     # The main algorithm.
 
     time_start = time.time()
@@ -78,12 +35,12 @@ def matrix_factorization(P, Q, R, K, steps=5000, alpha=0.07, beta=0.02):
         print("{:4d}".format(step), end=' ')
         print(time.strftime("%H:%M:%S", time.localtime()), end='')
 
-        # Iterates over the R matrix, only doing something if a rating has been given
+        # Iterates over the R matrix, only doing something if a rating has been given.
         for i in range(len(R)):
             for j in range(len(R[i])):
 
                 # If a rating has been given, calculate the error by getting the dot product of the user and item
-                # vectors in the latent vector spaces corresponding to the rating
+                # vectors in the latent vector spaces corresponding to the rating.
                 if R[i][j] > 0:
                     eij = R[i][j] - numpy.dot(P[i, :], Q[:, j])
 
@@ -94,147 +51,183 @@ def matrix_factorization(P, Q, R, K, steps=5000, alpha=0.07, beta=0.02):
                         P[i][k] = Pik + (alpha * ((2 * eij * Qkj) - (beta * Pik)))
                         Q[k][j] = Qkj + (alpha * ((2 * eij * Pik) - (beta * Qkj)))
 
+        error = 0
+        entries = 0
+
         # Calculate the average error of PxQ from R.
-        e = 0
-        g = 0
         for i in range(0, len(R)):
             for j in range(0, len(R[i])):
                 if R[i][j] > 0:
-                    e += pow(R[i][j] - numpy.dot(P[i, :], Q[:, j]), 2)
-                    g += 1
+                    error += pow(R[i][j] - numpy.dot(P[i, :], Q[:, j]), 2)
+                    entries += 1
                     for k in range(0, K):
-                        e += (beta / 2) * (pow(P[i][k], 2) + pow(Q[k][j], 2))
-        a_e = e / g
-        # If the error is arbitrarily small, stop execution to avoid useless running time.
-        print(" Error = " + "{:.2f}".format(float(e)) + " Average Error = " + "{:.2f}".format(float(a_e)))
+                        error += (beta / 2) * (pow(P[i][k], 2) + pow(Q[k][j], 2))
+        average_error = error / entries
 
-        if prev_e != 0 and prev_e - e < 0.1:
+        print(" Error = " + "{:.2f}".format(float(error)) + " Average Error = " + "{:.2f}".format(float(average_error)))
+
+        # If the change in error is arbitrarily small, stop execution to avoid useless running time.
+        if prev_error != 0 and prev_error - error < 0.1:
             break
 
-        prev_e = e
+        prev_error = error
 
     print("Elapsed time: " + time.strftime("%H:%M:%S", time.localtime(time.time() - time_start)))
 
     return P, Q.T
 
 
+# Calculates the biases of all users and movies, as well as the global average rating.
 def calculate_biases(R):
+    # Calculates the user biases.
     user_averages = []
     for i in range(len(R)):
-        sum = 0
+        total = 0
         count = 0
         for j in range(len(R[0])):
             if R[i][j] > 0:
-                sum += R[i][j]
+                total += R[i][j]
                 count += 1
 
         if not count == 0:
-            user_averages.insert(i, sum/count)
+            user_averages.insert(i, total / count)
         else:
             user_averages.insert(i, 0.0)
 
+    # Calculate the movie biases.
     movie_averages = []
     for j in range(len(R[0])):
-        sum = 0
+        total = 0
         count = 0
         for i in range(len(R)):
             if R[i][j] > 0:
-                sum += R[i][j]
+                total += R[i][j]
                 count += 1
 
         if not count == 0:
-            movie_averages.insert(i, sum / count)
+            movie_averages.insert(i, total / count)
         else:
             movie_averages.insert(i, 0.0)
 
-    sum = 0
+    # Calculate the global average rating.
+    total = 0
     count = 0
     for i in range(len(R)):
         for j in range(len(R[0])):
             if R[i][j] > 0:
-                sum += R[i][j]
+                total += R[i][j]
                 count += 1
     if not count == 0:
-        global_average = sum/count
+        global_average = total / count
     else:
         global_average = 0
 
     return global_average, movie_averages, user_averages
 
 
+# Adjusts the input matrix based on user and movie biases.
+def adjust_for_bias(R, global_average, movie_averages, user_averages):
+    adjusted_R = copy.deepcopy(R)
+    for i in range(len(R)):
+        for j in range(len(R[0])):
+            if adjusted_R[i][j] > 0:
+                adjusted_R[i][j] = R[i][j] - user_averages[i] - movie_averages[j] + global_average
+
+    return adjusted_R
+
+
+# Reverses the adjustments made on the input matrix for the output matrix.
+def readjust_for_bias(R, global_average, movie_averages, user_averages):
+    readjusted_R = copy.deepcopy(R)
+    for i in range(len(R)):
+        for j in range(len(R[0])):
+            readjusted_R[i][j] = R[i][j] + user_averages[i] + movie_averages[j] - global_average
+
+    return readjusted_R
+
+
+# Run the matrix factorization algorithm with all setup.
+def do_matrix_factorization(test_set, K=20, steps=1500, alpha=0.03, beta=0.02):
+    # Initialize matrices and values.
+    R = read_ratings(test_set)
+    P = numpy.random.rand(len(R), K)
+    Q = numpy.random.rand(len(R[0]), K)
+    global_average, movie_averages, user_averages = calculate_biases(R)
+    R = adjust_for_bias(R, global_average, movie_averages, user_averages)
+    R = numpy.array(R)
+
+    # Run algorithm on matrices.
+    new_P, new_Q = matrix_factorization(P, Q, R, K=K, steps=steps, alpha=alpha, beta=beta)
+
+    # Calculate recommendation matrix.
+    recommendations = numpy.dot(P, Q.T)
+
+    # write all matrices to files for inspection and saving purposes.
+    write_matrix(R, "R_original.data", test_set)
+    write_matrix(readjust_for_bias(recommendations, global_average, movie_averages, user_averages), "ratings.data", test_set)
+    write_matrix(recommendations, "unadjusted_ratings.data", test_set)
+    write_feature_matrix(new_P, "P.data", test_set)
+    write_feature_matrix(new_Q, "Q.data", test_set)
+
+
+# Algorithm to write a result matrix to a file.
+def write_matrix(m, file_name, test_set):
+    file = open("Output/" + test_set + "/" + file_name, "w")
+    file.write(" ID , " + ", ".join(str(x) for x in range(1, len(m[0]) + 1)) + "\n")
+    for i in range(0, len(m)):
+        file.write("{:>4d}, ".format(i + 1))
+        file.write(", ".join(["{: .2f}".format(x) for x in m[i]]))
+        file.write("\n")
+
+    if not file.closed:
+        file.close()
+
+
+# Algorithm to write a feature matrix to a file.
+def write_feature_matrix(m, file_name, test_set):
+    file = open("Output/" + test_set + "/" + file_name, "w")
+    for i in range(0, len(m)):
+        file.write("{:>4d}, ".format(i + 1))
+        file.write(", ".join(["{: .2f}".format(x) for x in m[i]]))
+        file.write("\n")
+
+    if not file.closed:
+        file.close()
+
+
+# Bound the result matrix so no value is above 5 or below 1.
+def bound_results(R, test_set):
+    bounded_R = copy.deepcopy(R)
+    for i in range(len(bounded_R)):
+        for j in range(len(bounded_R[0])):
+            if bounded_R[i][j] > 5:
+                bounded_R[i][j] = 5
+            elif bounded_R[i][j] < 1:
+                bounded_R[i][j] = 1
+
+    write_matrix(bounded_R, "bounded_ratings.data", test_set)
+
+
+# Calculate k recommendations for the user.
 def calculate_k_recommendations(user, k, test_set):
+    # Read matrices.
     R = read_ratings(test_set)
     P, Q = read_factor_matrices(test_set)
     P = numpy.array(P)
     Q = numpy.array(Q)
 
+    # Calculate averages.
     global_average, movie_averages, user_averages = calculate_biases(R)
 
-    ratings = []
+    # Calculate the ratings for all user/item pairs, and return the k highest ones.
+    recommendations = []
     for i in range(len(Q)):
         if R[user - 1][i] == 0.0:
-            ratings.append([i + 1, numpy.dot(P[user - 1, :], Q[i, :]) - global_average + movie_averages[i] + user_averages[user - 1]])
+            recommendations.append([i + 1, numpy.dot(P[user - 1, :], Q[i, :]) - global_average + movie_averages[i] + user_averages[user - 1]])
 
-    return sorted(ratings, key=lambda x: x[1], reverse=True)[:k]
+    return sorted(recommendations, key=lambda x: x[1], reverse=True)[:k]
 
-
-def adjust_for_bias(R, global_average, movie_averages, user_averages):
-    R_new = copy.deepcopy(R)
-    for i in range(len(R)):
-        for j in range(len(R[0])):
-            if R_new[i][j] > 0:
-                R_new[i][j] = R[i][j] - user_averages[i] - movie_averages[j] + global_average
-
-    return R_new
-
-
-def readjust_for_bias(R, global_average, movie_averages, user_averages):
-    R_new = copy.deepcopy(R)
-    for i in range(len(R)):
-        for j in range(len(R[0])):
-            R_new[i][j] = R[i][j] + user_averages[i] + movie_averages[j] - global_average
-
-    return R_new
-
-
-def bound_results(R_final, test_set):
-    R_bounded = copy.deepcopy(R_final)
-    for i in range(len(R_bounded)):
-        for j in range(len(R_bounded[0])):
-            if R_bounded[i][j] > 5:
-                R_bounded[i][j] = 5
-            elif R_bounded[i][j] < 1:
-                R_bounded[i][j] = 1
-
-    write_numpy_matrix(R_bounded, "bounded_ratings.data", test_set)
-
-
-def do_factorization(test_set, K=20, steps=1500, alpha=0.03, beta=0.02):
-    # Initialize matrices and values.
-    R = read_ratings(test_set)
-
-    P = numpy.random.rand(len(R), K)
-    Q = numpy.random.rand(len(R[0]), K)
-    global_average, movie_averages, user_averages = calculate_biases(R)
-
-    R = adjust_for_bias(R, global_average, movie_averages, user_averages)
-    R = numpy.array(R)
-
-    # Run algorithm on matrices.
-    nP, nQ = matrix_factorization(P, Q, R, K=K, steps=steps, alpha=alpha, beta=beta)
-
-    # Calculate recommendation and write it to recommendation file.
-    # calculate_recommendations(nP, nQ, R, test_set)
-
-    # Calculate and write all matrices to files for inspection and saving purposes.
-    R_final = numpy.dot(P, Q.T)
-    write_numpy_matrix(R, "R_original.data", test_set)
-    write_numpy_matrix(readjust_for_bias(R_final, global_average, movie_averages, user_averages), "ratings.data", test_set)
-    write_numpy_matrix(R_final, "unadjusted_ratings.data", test_set)
-    write_factor_matrix(nP, "P.data", test_set)
-    write_factor_matrix(nQ, "Q.data", test_set)
-
-
+# Dialogue to determine what test sets will be run.
 print("Do you want to run for all test sets? (Y/N)")
 s = input()
 if s == 'Y':
@@ -246,9 +239,11 @@ elif s == 'N':
 else:
     tests_to_run = [1]
 
+# Run the algorithms on all test sets.
 for i in tests_to_run:
-    do_factorization("Test" + str(i), K=20, steps=1000, alpha=0.03, beta=0.02)
+    do_matrix_factorization("Test" + str(i), K=20, steps=1000, alpha=0.03, beta=0.02)
 
+# Automatically run evaluation on the output of the algorithm.
 evaluator = Evaluation.RatingEvaluator(["Matrix Factorization V.2"], tests_to_run)
 evaluator.EvaluateAllAlgorithms()
 evaluator.LogResults("Evaluation Description: ")
