c85a1773a6bb472892895c0a985f1a98760617f6
moller93 <jensen.rasmusmoller@gmail.com>
2016-12-06 22:03:46 +0100

    Refactored Final Solution so it is a bit prettier. Have not changed any line of code, only moved them around.

--- RecommenderSystem/Final Solution/FinalSolution.py
+++ RecommenderSystem/Final Solution/FinalSolution.py
@@ -4,34 +4,17 @@ import DataAPI
 from ContentBased import calculate_recommendation_matrix
 from Division import recommend
 from PrecisionRecall import AveragePrecisionRecall
-test = 1
-test_set = "Test" + str(test)
-
-#KNN(test)
-head_ratings = DataAPI.read_recommendation_matrix("v2.0NearestNeighbour", test_set)
-
-#calculate_recommendation_matrix(test_set)
-tail_ratings = DataAPI.read_recommendation_matrix("Weighted Content Based", test_set)
 
 
-directory = test_set
-users = DataAPI.read_users_as_id_list()
-movies = DataAPI.read_movies_as_id_list()
-old_ratings = DataAPI.read_ratings(directory)
+def log(evaluation_result):
+    file = open("EvaluationLog.txt", "a")
+    file.write(evaluation_result)
 
-head_movies, tail_movies = get_head_and_tail(41)
-new_ratings = merge(head_movies, tail_movies, head_ratings, tail_ratings, users)
-
-recommendation = []
-
-for user in range(0, len(users)):
-    recommendation.insert(user, recommend(user, old_ratings, movies, new_ratings, users, 10))
-    print(user, ":", recommendation[user-1])
+    if not file.closed:
+        file.close()
 
 
-
-
-def isRelevant(rating, user, movie):
+def is_relevant(rating):
     if rating == 0:
         return None
     elif rating >= 4:
@@ -39,5 +22,35 @@ def isRelevant(rating, user, movie):
     else:
         return False
 
-print (AveragePrecisionRecall(recommendation, isRelevant))
-#kør precision recall på recomendationen
+
+def do_hybrid_recommendation(test):
+    test_set = "Test" + str(test)
+    users = DataAPI.read_users_as_id_list()
+    movies = DataAPI.read_movies_as_id_list()
+    ratings = DataAPI.read_ratings(test_set)
+
+    # Do K-Nearest Neigbour
+    KNN(test)
+    head_ratings = DataAPI.read_recommendation_matrix("v2.0NearestNeighbour", test_set)
+
+    # Do Content Based
+    calculate_recommendation_matrix(test_set)
+    tail_ratings = DataAPI.read_recommendation_matrix("Weighted Content Based", test_set)
+
+    # Merge results
+    head_movies, tail_movies = get_head_and_tail(41)
+    new_ratings = merge(head_movies, tail_movies, head_ratings, tail_ratings, users)
+
+    # Generate recommendations
+    recommendations = []
+    for user in range(0, len(users)):
+        recommendations.insert(user, recommend(user, ratings, movies, new_ratings, users, 10))
+        print(user, ":", recommendations[user - 1])
+
+    return recommendations
+
+for i in [1, 2, 3, 4, 5]:
+    recommendations = do_hybrid_recommendation(i)
+    result = AveragePrecisionRecall(recommendations, is_relevant)
+    print(result)
+    log(result)
