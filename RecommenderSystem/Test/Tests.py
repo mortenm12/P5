204ef2324de80048cb6e41749b86e0cb3dcba18a
luraman <ponifi3d@gmail.com>
2016-12-07 14:12:38 +0100

    Added PrecisionRecall

--- RecommenderSystem/Test/Tests.py
+++ RecommenderSystem/Test/Tests.py
@@ -4,6 +4,7 @@ import MatrixFactorization as MF
 import ContentBased as CB
 import v20NearestNeighbour as NNCV
 import v21NearestNeighbour as NNPC
+import PrecisionRecall as PR
 import DataAPI
 import copy
 
@@ -36,6 +37,40 @@ class Test(unittest.TestCase):
          DataAPI.Movie(4, None, [1, 2, 4, 9, 15]),
          DataAPI.Movie(5, None, [1, 16])]
     U_obj = DataAPI.add_rating_metrics_to_users(M_obj, U_obj, R)
+
+    def test_precision_recall(self):
+        PR.all_ratings = Test.R
+        expected = [
+            (1, 1),
+            (0.25, 1),
+            (0, None),
+            (1, 1),
+            (0, None)
+        ]
+
+        def precision_recall_from_confusion_matrix(confusion_matrix):
+            if confusion_matrix["TruePositive"] + confusion_matrix["FalsePositive"] > 0:
+                precision = PR.calculate_precision(confusion_matrix)
+            else:
+                precision = None
+            if confusion_matrix["TruePositive"] + confusion_matrix["FalseNegative"] > 0:
+                recall = PR.calculate_recall(confusion_matrix)
+            else:
+                recall = None
+            return (precision, recall)
+
+        def predicate(rating, user, movie):
+            if rating == 0:
+                return None
+            elif rating >= 4:
+                return True
+            else:
+                return False
+
+        result = []
+        for test in range(0,5):
+            result.append(precision_recall_from_confusion_matrix(PR.confusion_matrix_generator_from_predicate(predicate)(range(0, 5), test)))
+            self.assertEqual(result[test], expected[test])
 
     def test_matrix_factorization(self):
         Pout = [[2.18829953982, 1.09414976991],
