841616fdab068667f8a2ef9892272f8e663659db
luraman <ponifi3d@gmail.com>
2016-12-08 15:48:50 +0100

    Remove dependency between precision and recall

--- RecommenderSystem/Evaluation/PrecisionRecall.py
+++ RecommenderSystem/Evaluation/PrecisionRecall.py
@@ -15,14 +15,14 @@ class NoValidEntriesError(Exception):
 #             Returns weather a recommendation should be recommended. If that's undecidable, returns None.
 # return - A confusion-matrix generator. (Defined inside function)
 #
-def confusion_matrix_generator_from_predicate(predicate):
+def confusion_matrix_generator_from_predicate(predicate, all_ratings):
     relevant_count = []
     irrelevant_count = []
     relevance_matrix = []
 
     # Count how many ratings does/doesn't satisfy the predicate.
     # Also record the results in correctnessMatrix in case of a slow predicate.
-    for user, userRatings in enumerate(AllRatings):
+    for user, userRatings in enumerate(all_ratings):
         relevance_matrix.append([])
         relevant_count.append(0)
         irrelevant_count.append(0)
@@ -63,12 +63,61 @@ def confusion_matrix_generator_from_predicate(predicate):
 
 
 def calculate_precision(confusion_matrix):
+    if confusion_matrix["TruePositive"] + confusion_matrix["FalsePositive"] == 0:
+        return None
     return float(confusion_matrix["TruePositive"]) / (confusion_matrix["TruePositive"] + confusion_matrix["FalsePositive"])
 
 
 def calculate_recall(confusion_matrix):
+    if confusion_matrix["TruePositive"] + confusion_matrix["FalseNegative"] == 0:
+        return None
     return float(confusion_matrix["TruePositive"]) / (confusion_matrix["TruePositive"] + confusion_matrix["FalseNegative"])
 
+def calculate_average_recall(recommendation_lists, confusion_matrix_generator):
+    recallSum = 0.0
+    valid_entries = 0
+
+    for user, recommendations in enumerate(recommendation_lists):
+        # Make a confusion matrix for the current recommendationlist
+        confusion_matrix = confusion_matrix_generator(recommendations, user)
+
+        # Calculate the results
+        currentRecall = calculate_recall(confusion_matrix)
+        # If the recommendationlist is to be considered
+        if currentRecall is not None:
+            # Add the results
+            recallSum += currentRecall
+            # Increment the valid entries count
+            valid_entries += 1
+
+    # Prevent taking an average over no entries.
+    if valid_entries == 0:
+        raise NoValidEntriesError
+    # Return the averages
+    return recallSum / valid_entries
+
+def calculate_average_precision(recommendation_lists, confusion_matrix_generator):
+    precisionSum = 0.0
+    valid_entries = 0
+
+    for user, recommendations in enumerate(recommendation_lists):
+        # Make a confusion matrix for the current recommendationlist
+        confusion_matrix = confusion_matrix_generator(recommendations, user)
+
+        # Calculate the results
+        currentPrecision = calculate_precision(confusion_matrix)
+        # If the recommendationlist is to be considered
+        if currentPrecision is not None:
+            # Add the results
+            precisionSum += currentPrecision
+            # Increment the valid entries count
+            valid_entries += 1
+
+    # Prevent taking an average over no entries.
+    if valid_entries == 0:
+        raise NoValidEntriesError
+    # Return the averages
+    return precisionSum / valid_entries
 
 # average_precision_recall(recommendationLists, predicate):
 # Calculates an average precision recall.
@@ -81,25 +130,7 @@ def calculate_recall(confusion_matrix):
 #             Returns weather a recommendation should be recommended. If that's undecidable, returns None.
 # return - The average precision and the average recall.
 def average_precision_recall(recommendation_lists, predicate):
-    precision = 0.0
-    recall = 0.0
-    valid_entries = 0
-
-    # Make the confusion matrix generator for the predicate
-    confusion_matrix_generator = confusion_matrix_generator_from_predicate(predicate)
-    for user, recommendations in enumerate(recommendation_lists):
-        # Make a confusion matrix for the current recommendationlist
-        confusion_matrix = confusion_matrix_generator(recommendations, user)
-        # If the recommendationlist is to be considered
-        if confusion_matrix["TruePositive"] + confusion_matrix["FalsePositive"] > 0:
-            # Add the results
-            precision += calculate_precision(confusion_matrix)
-            recall += calculate_recall(confusion_matrix)
-            # Increment the valid entries count
-            valid_entries += 1
-
-    # Prevent taking an average over no entries.
-    if valid_entries == 0:
-        raise NoValidEntriesError
-    # Return the averages
-    return precision / valid_entries, recall / valid_entries
+    confusion_matrix_generator = confusion_matrix_generator_from_predicate(predicate, AllRatings)
+    average_precision = calculate_average_precision(recommendation_lists, confusion_matrix_generator)
+    average_recall = calculate_average_recall(recommendation_lists, confusion_matrix_generator)
+    return average_precision, average_recall
